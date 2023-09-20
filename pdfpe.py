647d6fc0e1aa1231270567d98629b553772d64d1
Nic Bolton <83439607+bolst@users.noreply.github.com>
2023-09-20 11:40:45 -0400

    add comments and change return to sys.exit()

--- pdfpe.py
+++ pdfpe.py
@@ -4,18 +4,22 @@ import re
 import fitz
 import json
 
-#COURSE_CODE_RGX = r'^[A-Z]{4}-\d{4}\.$'
+# COURSE_CODE_RGX = r'^[A-Z]{4}-\d{4}\.$'
 
+# write 'data' to file named filename
 def write_json(filename, data):
     with open(filename, 'w') as f:
         json.dump(data, f)
 
+
 def main():
     
+    # handle incorrect input/usage
     if len(sys.argv) != 3:
         print("Usage: python pdfpe.py <file> <pattern>")
         sys.exit(1)
         
+    # extract inputs
     filename = sys.argv[1]
     pattern = sys.argv[2]
     
@@ -27,17 +31,28 @@ def main():
         print("File must be a PDF")
         sys.exit(1)
         
+    # open pdf using fitz
     doc = fitz.open(filename)
     
+    # empty list for words to return
     retval = []
+
+    # iterate through each page in pdf
     for page in doc:
+        
+        # get text on page
         text = page.get_text()
+
+        # iterate through each word on page
         for word in text.split():
+
+            # add word to 'retval' if word matches inputted regex
             if re.search(pattern, word) and word not in retval:
                     retval.append(word)
     
+    # write to file
     write_json(filename[:-4] + "-xt.json", retval)
-    return 0
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
